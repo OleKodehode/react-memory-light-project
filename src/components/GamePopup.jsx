cc57f2f22eaf71efb7f64b4e27efd69823dbb8a8
Ole Kodehode <ole.kodehode@gmail.com>
2025-10-06 14:22:40 +0200

    styling

--- src/components/GamePopup.jsx
+++ src/components/GamePopup.jsx
@@ -1,7 +1,7 @@
 export default function GamePopup({ start, gameInfo }) {
   return (
     <div className="fixed inset-0 bg-black/50 backdrop-blur-xs z-40">
-      <dialog className="fixed w-1/2 top-10 left-50 h-auto z-50 p-10 rounded-2xl flex flex-col justify-center items-center gap-8 dark:bg-slate-900 dark:text-zinc-200">
+      <dialog className="fixed w-1/2 top-10 left-50 lg:top-40 lg:left-100 h-auto z-50 p-10 rounded-2xl flex flex-col justify-center items-center gap-8 dark:bg-slate-900 dark:text-zinc-200">
         <h2 className="text-xl font-bold">Match 2 Memory Game</h2>
         <p>
           A classic game of Match 2
@@ -15,7 +15,7 @@ export default function GamePopup({ start, gameInfo }) {
         </p>
         <button
           onClick={() => start()}
-          className="p-2 dark:bg-slate-700 rounded-lg"
+          className="transition-colors duration-200 p-2 dark:bg-slate-700 rounded-lg hover:dark:bg-slate-500"
         >
           Start the game
         </button>
